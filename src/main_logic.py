6d38191436e5c14d87e91de0df64859a392927ea
AyushStarGupta <37546890+AyushStarGupta@users.noreply.github.com>
2021-03-29 16:25:50 +0530

    If it's stupid and it works, it ain't stupid

---
+++ src/main_logic.py
@@ -0,0 +1,17 @@
+main_dict = {'Giza': {'Big Banks': 2, 'Hospitals': 3, 'Rich Population': 3, 'Old Artefacts': 9, 'Crime Rate': 8,
+                      'Black Market': 1},
+             'Agra': {'Big Banks': 5, 'Hospitals': 3, 'Rich Population': 6, 'Old Artefacts': 8, 'Crime Rate': 3,
+                      'Black Market': 0},
+             'Paris': {'Big Banks': 9, 'Hospitals': 7, 'Rich Population': 7, 'Old Artefacts': 7, 'Crime Rate': 6,
+                       'Black Market': 1},
+             'New York': {'Big Banks': 10, 'Hospitals': 5, 'Rich Population': 8, 'Old Artefacts': 1, 'Crime Rate': 3,
+                          'Black Market': 0},
+             'Rome': {'Big Banks': 9, 'Hospitals': 3, 'Rich Population': 3, 'Old Artefacts': 8, 'Crime Rate': 7,
+                      'Black Market': 1}}
+
+health = 100
+prev_steal = None
+last_seen = None
+coins = 0
+skill_level = 1
+
