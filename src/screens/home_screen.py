c7b19343c58c5dc8a73ceb6cbfc3bcb3921f7ebd
Pranjal Rastogi <pranjal_j_rastogi@yahoo.in>
2021-03-30 17:53:41 +0530

    added settings screen and more buttons, that actually do nothing much

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -5,6 +5,7 @@ import pygame
 
 from . import chase
 from . import cities
+from . import settings
 from ..utils import constants as consts
 from ..utils.widgets import TextButton
 from ..utils import colors
@@ -22,6 +23,21 @@ def home_screen():
                                                                 (consts.SCREEN_HEIGHT / 2) + 160),
                               width=200, height=40, fg_color=colors.WHITE_COLOR, bg_color=colors.BLACK_COLOR,
                               font=pygame.font.Font('freesansbold.ttf', 30), text='Chase')
+
+    play_button = TextButton(surface=consts.MAIN_DISPLAY, pos=((consts.SCREEN_WIDTH / 2) - 400,
+                                                               (consts.SCREEN_HEIGHT / 2) + 0),
+                             width=200, height=40, fg_color=colors.WHITE_COLOR, bg_color=colors.BLACK_COLOR,
+                             font=pygame.font.Font('freesansbold.ttf', 30), text='Actually Play')
+
+    shop_button = TextButton(surface=consts.MAIN_DISPLAY, pos=((consts.SCREEN_WIDTH / 2) - 400,
+                                                               (consts.SCREEN_HEIGHT / 2) - 300),
+                             width=200, height=40, fg_color=colors.WHITE_COLOR, bg_color=colors.BLACK_COLOR,
+                             font=pygame.font.Font('freesansbold.ttf', 30), text='Shop')
+
+    settings_button = TextButton(surface=consts.MAIN_DISPLAY, pos=((consts.SCREEN_WIDTH / 2) + 100,
+                                                                   (consts.SCREEN_HEIGHT / 2) - 300),
+                                 width=200, height=40, fg_color=colors.WHITE_COLOR, bg_color=colors.BLACK_COLOR,
+                                 font=pygame.font.Font('freesansbold.ttf', 30), text='Settings')
 
     # the main game loop, looped every frame, looped every clock.tick(TICK_RATE)
     is_game_over = False
@@ -43,6 +59,7 @@ def home_screen():
                 return cities.play()
         else:
             city_button.toggle_bg(colors.BLACK_COLOR)
+
         if chase_button.hovered:
             chase_button.toggle_bg(colors.BROWN_COLOR)
             if mouse_down:
@@ -50,6 +67,30 @@ def home_screen():
                 return chase.play()
         else:
             chase_button.toggle_bg(colors.BLACK_COLOR)
+
+        if play_button.hovered:
+            play_button.toggle_bg(colors.BROWN_COLOR)
+            if mouse_down:
+                play_button.toggle_bg(colors.BROWN_COLOR)
+                return cities.play()
+        else:
+            play_button.toggle_bg(colors.BLACK_COLOR)
+
+        if shop_button.hovered:
+            shop_button.toggle_bg(colors.BROWN_COLOR)
+            if mouse_down:
+                shop_button.toggle_bg(colors.BROWN_COLOR)
+                pass  # return shop.play()
+        else:
+            shop_button.toggle_bg(colors.BLACK_COLOR)
+
+        if settings_button.hovered:
+            settings_button.toggle_bg(colors.BROWN_COLOR)
+            if mouse_down:
+                settings_button.toggle_bg(colors.BROWN_COLOR)
+                return settings.play()
+        else:
+            settings_button.toggle_bg(colors.BLACK_COLOR)
 
         # update all the things in game
         pygame.display.update()
---
+++ src/screens/settings.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2021 Ayush Gupta, Kartikey Pandey, Pranjal Rastogi, Sohan Varier, Shreyansh Kumar
+# Author: Pranjal Rastogi
+
+import pygame
+
+from . import chase
+from . import cities
+from ..utils import constants as consts
+from ..utils.widgets import TextButton
+from ..utils import colors
+from .. import utils
+
+pygame.init()
+
+
+# settings screen
+def play():
+    utils.constants.MAIN_DISPLAY.fill((0, 255, 255))
+    # the main game loop, looped every frame, looped every clock.tick(TICK_RATE)
+    is_game_over = False
+    while not is_game_over:
+
+        mouse_down = False
+        # gets all the events occurring every frame, which can be mouse movement, mouse click, etc.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                # quit game if QUIT is invoked
+                is_game_over = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_down = True
+
+        # update all the things in game
+        pygame.display.update()
+        consts.CLOCK.tick(consts.TICK_RATE)
